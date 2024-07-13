ba2966ef1343839b25cd5230e1360c78abe98f67
Nikita Biichuk <niktestpython@gmail.com>
2024-07-13 23:25:25 +0300

    Upodate model

--- lib/database/patient.model.ts
+++ lib/database/patient.model.ts
@@ -6,58 +6,58 @@ export interface IPatient extends Document {
   fullName: string;
   image: string;
   email: string;
-  phone: string;
-  birthDate: Date;
-  gender: string;
-  address: string;
-  occupation: string;
-  emergencyContactName: string;
-  emergencyContactNumber: string;
-  primaryPhysician: string;
-  insuranceProvider: string;
-  insurancePolicyNumber: string;
+  phone?: string;
+  birthDate?: Date;
+  gender?: string;
+  address?: string;
+  occupation?: string;
+  emergencyContactName?: string;
+  emergencyContactNumber?: string;
+  primaryPhysician?: string;
+  insuranceProvider?: string;
+  insurancePolicyNumber?: string;
   allergies?: string;
   currentMedication?: string;
   familyMedicalHistory?: string;
   pastMedicalHistory?: string;
-  identificationType: string;
-  identificationNumber: string;
-  identificationDocumentUrl: string;
-  treatmentConsent: boolean;
-  disclosureConsent: boolean;
-  privacyConsent: boolean;
+  identificationType?: string;
+  identificationNumber?: string;
+  identificationDocumentUrl?: string;
+  treatmentConsent?: boolean;
+  disclosureConsent?: boolean;
+  privacyConsent?: boolean;
   createdAt: Date;
   updatedAt: Date;
-  appointments: Schema.Types.ObjectId[];
+  appointments?: Schema.Types.ObjectId[];
 }
 
 // Define the Patient schema with appropriate fields and types
 const PatientSchema = new Schema<IPatient>(
   {
-    clerkId: { type: String, required: true },
-    fullName: { type: String, required: true },
-    image: { type: String, required: true },
-    email: { type: String, required: true },
-    phone: { type: String, required: true },
-    birthDate: { type: Date, required: true },
-    gender: { type: String, required: true },
-    address: { type: String, required: true },
-    occupation: { type: String, required: true },
-    emergencyContactName: { type: String, required: true },
-    emergencyContactNumber: { type: String, required: true },
-    primaryPhysician: { type: String, required: true },
-    insuranceProvider: { type: String, required: true },
-    insurancePolicyNumber: { type: String, required: true },
+    clerkId: { type: String },
+    fullName: { type: String },
+    image: { type: String },
+    email: { type: String },
+    phone: { type: String },
+    birthDate: { type: Date },
+    gender: { type: String },
+    address: { type: String },
+    occupation: { type: String },
+    emergencyContactName: { type: String },
+    emergencyContactNumber: { type: String },
+    primaryPhysician: { type: String },
+    insuranceProvider: { type: String },
+    insurancePolicyNumber: { type: String },
     allergies: { type: String },
     currentMedication: { type: String },
     familyMedicalHistory: { type: String },
     pastMedicalHistory: { type: String },
-    identificationType: { type: String, required: true },
-    identificationNumber: { type: String, required: true },
-    identificationDocumentUrl: { type: String, required: true },
-    treatmentConsent: { type: Boolean, required: true },
-    disclosureConsent: { type: Boolean, required: true },
-    privacyConsent: { type: Boolean, required: true },
+    identificationType: { type: String },
+    identificationNumber: { type: String },
+    identificationDocumentUrl: { type: String },
+    treatmentConsent: { type: Boolean },
+    disclosureConsent: { type: Boolean },
+    privacyConsent: { type: Boolean },
     appointments: [{ type: Schema.Types.ObjectId, ref: "Appointment" }],
   },
   {
